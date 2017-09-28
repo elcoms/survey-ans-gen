577d04cf4bd5d989be1c46deac15cde356e59448
Elcoms <elcomsortsort@gmail.com>
2017-09-28 23:25:20 +0800

    Merge fix_issues (#11)
    * Fix Issue #8

--- js/CCSscript.js
+++ js/CCSscript.js
@@ -1,29 +1,26 @@
 var lessons = [];
 var timing = [];
 
-function copied(event){
-
-  var element = event.target;
-
-  document.getElementById("cTopic").innerHTML = "Generate another answer!";
-  document.getElementById("copy").innerHTML = "Copied!";
-  document.getElementById("copy").style.backgroundColor = "#1f63d6";
+function copied(){
+  document.getElementById("bTopic").innerHTML = "Copy";
+  document.getElementById("bTopic").style.backgroundColor = "#8edbf9";
 }
 
 function output(event){
 
   var element = event.target;
+
   //randomize and output
   var randomNum = 0, randomNum2 = 0;
 
   randomNum = Math.floor(Math.random() * (lessons.length-2) + 0);
   randomNum2 = Math.floor(Math.random() * (timing.length-2) + 0);
-  
+
   document.getElementById("topic").value = lessons[randomNum] + timing[randomNum2];
-  document.getElementById("cTopic").innerHTML = "Waiting for you to copy. . .";
-  document.getElementById("cTopic").style.color = 'black';
-  document.getElementById("copy").innerHTML = "Copy";
-  document.getElementById("copy").style.backgroundColor = "#8edbf9";
+  document.getElementById("cTopic").innerHTML = "Generate another answer!";
+  document.getElementById("cTopic").style.color = '#'+(0x1000000+(Math.random())*0xffffff).toString(16).substr(1,6);
+  document.getElementById("bTopic").innerHTML = "Copied!";
+  document.getElementById("bTopic").style.backgroundColor = "#1f63d6";
 }
 
 function getData(callback, file)
